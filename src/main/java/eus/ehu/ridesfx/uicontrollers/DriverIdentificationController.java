f00127cc4151e1cb41061b9a29d4b01c38bcb2e8
Maddi <maguinaga005@ikasle.ehu.eus>
2024-03-13 11:26:39 +0100

    DriverIdentification.fxml and DriverIdentificationController added

---
+++ src/main/java/eus/ehu/ridesfx/uicontrollers/DriverIdentificationController.java
@@ -0,0 +1,25 @@
+package eus.ehu.ridesfx.uicontrollers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class DriverIdentificationController {
+
+    @FXML
+    private TextField driverEmail;
+
+    @FXML
+    private TextField driverPassword;
+
+    @FXML
+    void SignInBottom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SignUpBottom(ActionEvent event) {
+
+    }
+
+}
